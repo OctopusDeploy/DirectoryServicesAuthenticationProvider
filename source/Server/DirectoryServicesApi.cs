148ca3c588ebe0eb059e7e774366781fb3e1c3f1
Frank Lin <frank.lin@octopus.com>
2020-07-30 14:01:28 +1000

    Fix path

--- source/Server/DirectoryServicesApi.cs
+++ source/Server/DirectoryServicesApi.cs
@@ -6,8 +6,8 @@ namespace Octopus.Server.Extensibility.Authentication.DirectoryServices
 {
     class DirectoryServicesApi : RegistersEndpoints
     {
-        public const string ApiExternalGroupsSearch = "/api/externalgroups/directoryServices{?partialName}";
-        public const string ApiExternalUsersSearch = "/api/externalusers/directoryServices{?partialName}";
+        public const string ApiExternalGroupsSearch = "/externalgroups/directoryServices{?partialName}";
+        public const string ApiExternalUsersSearch = "/externalusers/directoryServices{?partialName}";
 
         public DirectoryServicesApi()
         {
