65817635b51c5bd6e37bc866edd7518c78e80757
Mark Coafield <mark.coafield@octopus.com>
2021-05-20 17:02:56 +1000

    Prevented Negotiate being used with HTTP.sys

--- source/Server/DirectoryServicesApplicationBuilderContributor.cs
+++ source/Server/DirectoryServicesApplicationBuilderContributor.cs
@@ -1,13 +1,24 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Octopus.Server.Extensibility.Extensions;
+using Octopus.Server.Extensibility.HostServices.Web;
 
 namespace Octopus.Server.Extensibility.Authentication.DirectoryServices
 {
     public class DirectoryServicesApplicationBuilderContributor : IContributeToType<IApplicationBuilder>
     {
+        readonly IWebPortalConfigurationStore configuration;
+
+        public DirectoryServicesApplicationBuilderContributor(IWebPortalConfigurationStore configuration)
+        {
+            this.configuration = configuration;
+        }
+
         public void ContributeTo(IApplicationBuilder instance)
         {
-            instance.UseAuthentication(); //
+            if (configuration.GetWebServer() == WebServer.Kestrel)
+            {
+                instance.UseAuthentication();
+            }
         }
     }
 }
--- source/Server/DirectoryServicesServiceCollectionContributor.cs
+++ source/Server/DirectoryServicesServiceCollectionContributor.cs
@@ -1,14 +1,25 @@
 ﻿using Microsoft.AspNetCore.Authentication.Negotiate;
 using Microsoft.Extensions.DependencyInjection;
 using Octopus.Server.Extensibility.Extensions;
+using Octopus.Server.Extensibility.HostServices.Web;
 
 namespace Octopus.Server.Extensibility.Authentication.DirectoryServices
 {
     public class DirectoryServicesServiceCollectionContributor : IContributeToType<IServiceCollection>
     {
+        readonly IWebPortalConfigurationStore configuration;
+
+        public DirectoryServicesServiceCollectionContributor(IWebPortalConfigurationStore configuration)
+        {
+            this.configuration = configuration;
+        }
+
         public void ContributeTo(IServiceCollection instance)
         {
-            instance.AddAuthentication(NegotiateDefaults.AuthenticationScheme).AddNegotiate();
+            if (configuration.GetWebServer() == WebServer.Kestrel)
+            {
+                instance.AddAuthentication(NegotiateDefaults.AuthenticationScheme).AddNegotiate();
+            }
         }
     }
 }
