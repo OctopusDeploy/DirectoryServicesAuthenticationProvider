cb65d80825ffe16b14c627cbed6bad1ebda7c567
Mark Coafield <mark.coafield@octopus.com>
2021-05-20 14:20:32 +1000

    Added contributors to container

--- source/Server/DirectoryServicesExtension.cs
+++ source/Server/DirectoryServicesExtension.cs
@@ -1,4 +1,5 @@
 ﻿using Autofac;
+using Microsoft.AspNetCore.Builder;
 using Octopus.Server.Extensibility.Authentication.DirectoryServices.Configuration;
 using Octopus.Server.Extensibility.Authentication.DirectoryServices.DirectoryServices;
 using Octopus.Server.Extensibility.Authentication.DirectoryServices.Identities;
@@ -21,72 +22,82 @@ namespace Octopus.Server.Extensibility.Authentication.DirectoryServices
     {
         public virtual void Load(ContainerBuilder builder)
         {
-            builder.RegisterType<DirectoryServicesConfigurationMapping>().As<IConfigurationDocumentMapper>().InstancePerDependency();
-            
+            builder.RegisterType<DirectoryServicesConfigurationMapping>().As<IConfigurationDocumentMapper>()
+                .InstancePerDependency();
+
             builder.RegisterType<DatabaseInitializer>().As<IExecuteWhenDatabaseInitializes>().InstancePerDependency();
-            
+
             builder.RegisterType<IdentityCreator>().As<IIdentityCreator>().SingleInstance();
-            
+
             builder.RegisterType<DirectoryServicesConfigurationStore>()
                 .As<IDirectoryServicesConfigurationStore>()
                 .InstancePerDependency();
-            
-             builder.RegisterType<DirectoryServicesConfigurationSettings>()
-                 .As<IDirectoryServicesConfigurationSettings>()
-                 .As<IHasConfigurationSettings>()
-                 .As<IHasConfigurationSettingsResource>()
-                 .As<IContributeMappings>()
-                 .InstancePerDependency();
-            
-             builder.RegisterType<DirectoryServicesUserCreationFromPrincipal>().AsSelf().InstancePerDependency();
-            
-             builder.RegisterType<DirectoryServicesContextProvider>().As<IDirectoryServicesContextProvider>().InstancePerDependency();
-             builder.RegisterType<DirectoryServicesObjectNameNormalizer>().As<IDirectoryServicesObjectNameNormalizer>().InstancePerDependency();
-             builder.RegisterType<UserPrincipalFinder>().As<IUserPrincipalFinder>().InstancePerDependency();
-             builder.RegisterType<DirectoryServicesExternalSecurityGroupLocator>()
-                 .As<IDirectoryServicesExternalSecurityGroupLocator>()
-                 .As<ICanSearchExternalGroups>()
-                 .InstancePerDependency();
-            
-             builder.RegisterType<DirectoryServicesService>()
-                 .As<IDirectoryServicesService>()
-                 .InstancePerDependency();
-            
-             builder.RegisterType<DirectoryServicesCredentialValidator>()
-                 .As<IDirectoryServicesCredentialValidator>()
-                 .As<IDoesBasicAuthentication>()
-                 .InstancePerDependency();
-            
-             builder.RegisterType<GroupRetriever>()
-                 .As<IExternalGroupRetriever>()
-                 .InstancePerDependency();
-            
-             builder.RegisterType<UserSearch>().As<ICanSearchExternalUsers>().As<ICanSearchActiveDirectoryUsers>().InstancePerDependency();
-             builder.RegisterType<UserMatcher>().As<ICanMatchExternalUser>().InstancePerDependency();
-             
-             builder.RegisterType<DirectoryServicesHomeLinksContributor>().As<IHomeLinksContributor>().InstancePerDependency();
-            
-             builder.RegisterType<DirectoryServicesConfigureCommands>()
-                 .As<IContributeToConfigureCommand>()
-                 .InstancePerDependency();
-            
-             builder.RegisterType<DirectoryServicesStaticContentFolders>().As<IContributesStaticContentFolders>().InstancePerDependency();
-            
-             builder.RegisterType<ListSecurityGroupsAction>().AsSelf().InstancePerDependency();
-             builder.RegisterType<UserLookupAction>().AsSelf().InstancePerDependency();
 
-             builder.RegisterType<DirectoryServicesAuthenticationProvider>()
-                 .As<IAuthenticationProvider>()
-                 .As<IAuthenticationProviderWithGroupSupport>()
-                 .As<IContributesCSS>()
-                 .As<IContributesJavascript>()
-                 .As<IUseAuthenticationIdentities>()
-                 .AsSelf()
-                 .InstancePerDependency();
+            builder.RegisterType<DirectoryServicesConfigurationSettings>()
+                .As<IDirectoryServicesConfigurationSettings>()
+                .As<IHasConfigurationSettings>()
+                .As<IHasConfigurationSettingsResource>()
+                .As<IContributeMappings>()
+                .InstancePerDependency();
 
-            builder.RegisterType<IntegratedAuthenticationHandler>().As<IIntegratedAuthenticationHandler>().InstancePerDependency();
+            builder.RegisterType<DirectoryServicesUserCreationFromPrincipal>().AsSelf().InstancePerDependency();
+
+            builder.RegisterType<DirectoryServicesContextProvider>().As<IDirectoryServicesContextProvider>()
+                .InstancePerDependency();
+            builder.RegisterType<DirectoryServicesObjectNameNormalizer>().As<IDirectoryServicesObjectNameNormalizer>()
+                .InstancePerDependency();
+            builder.RegisterType<UserPrincipalFinder>().As<IUserPrincipalFinder>().InstancePerDependency();
+            builder.RegisterType<DirectoryServicesExternalSecurityGroupLocator>()
+                .As<IDirectoryServicesExternalSecurityGroupLocator>()
+                .As<ICanSearchExternalGroups>()
+                .InstancePerDependency();
+
+            builder.RegisterType<DirectoryServicesService>()
+                .As<IDirectoryServicesService>()
+                .InstancePerDependency();
+
+            builder.RegisterType<DirectoryServicesCredentialValidator>()
+                .As<IDirectoryServicesCredentialValidator>()
+                .As<IDoesBasicAuthentication>()
+                .InstancePerDependency();
+
+            builder.RegisterType<GroupRetriever>()
+                .As<IExternalGroupRetriever>()
+                .InstancePerDependency();
+
+            builder.RegisterType<UserSearch>().As<ICanSearchExternalUsers>().As<ICanSearchActiveDirectoryUsers>()
+                .InstancePerDependency();
+            builder.RegisterType<UserMatcher>().As<ICanMatchExternalUser>().InstancePerDependency();
+
+            builder.RegisterType<DirectoryServicesHomeLinksContributor>().As<IHomeLinksContributor>()
+                .InstancePerDependency();
+
+            builder.RegisterType<DirectoryServicesConfigureCommands>()
+                .As<IContributeToConfigureCommand>()
+                .InstancePerDependency();
+
+            builder.RegisterType<DirectoryServicesStaticContentFolders>().As<IContributesStaticContentFolders>()
+                .InstancePerDependency();
+
+            builder.RegisterType<ListSecurityGroupsAction>().AsSelf().InstancePerDependency();
+            builder.RegisterType<UserLookupAction>().AsSelf().InstancePerDependency();
+
+            builder.RegisterType<DirectoryServicesAuthenticationProvider>()
+                .As<IAuthenticationProvider>()
+                .As<IAuthenticationProviderWithGroupSupport>()
+                .As<IContributesCSS>()
+                .As<IContributesJavascript>()
+                .As<IUseAuthenticationIdentities>()
+                .AsSelf()
+                .InstancePerDependency();
+
+            builder.RegisterType<IntegratedAuthenticationHandler>().As<IIntegratedAuthenticationHandler>()
+                .InstancePerDependency();
             builder.RegisterType<IntegratedAuthenticationHost>().As<IShareWebHostLifetime>().SingleInstance();
-            builder.RegisterType<IntegratedChallengeCoordinator>().As<IIntegratedChallengeCoordinator>().SingleInstance();
+            builder.RegisterType<IntegratedChallengeCoordinator>().As<IIntegratedChallengeCoordinator>()
+                .SingleInstance();
+
+            builder.RegisterType<DirectoryServicesApplicationBuilderContributor>().As<IContributeToType<IApplicationBuilder>>();
         }
     }
 }
