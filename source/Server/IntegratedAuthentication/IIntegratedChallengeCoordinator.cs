c816e1545d0511210d81a7f762e928e8f6de045b
Nelson Susanto <nelson.susanto@octopus.com>
2022-10-28 15:51:45 +1100

    Validate redirect uri on challenge failed (#82)
    * Validate redirect uri on challenge failed as well
    
    * Use variable and set challenge completed on safe redirect
    
    * Use a variable for redirectUrl

--- source/Server/IntegratedAuthentication/IIntegratedChallengeCoordinator.cs
+++ source/Server/IntegratedAuthentication/IIntegratedChallengeCoordinator.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Http;
+using Octopus.Diagnostics;
+using Octopus.Server.Extensibility.Authentication.HostServices;
 using Octopus.Server.Extensibility.Authentication.Resources;
 
 namespace Octopus.Server.Extensibility.Authentication.DirectoryServices.IntegratedAuthentication
 {
     interface IIntegratedChallengeCoordinator
     {
-        IntegratedChallengeTrackerStatus SetupResponseIfChallengeHasNotSucceededYet(HttpContext context, LoginState? state);
+        IntegratedChallengeTrackerStatus SetupResponseIfChallengeHasNotSucceededYet(HttpContext context, LoginState? state, IAuthenticationConfigurationStore authenticationConfigurationStore, ISystemLog log);
     }
 }
